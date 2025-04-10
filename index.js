0c00ac8d762bd5c3eaa0ef8284c69d3830902a78
dinesh-kumar19 <dineshkumarsdk03@gmail.com>
2025-04-10 15:34:06 +0530

    updated to online connection

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ cron.schedule('0 0 * * *', () => {
         }
     );
 });
-const port = process.env.APP_PORT || 3000;
+const port = process.env.PORT || 3000;
 // Listen port
 app.listen(port, () => {
     console.log("Server is running on port",port);
