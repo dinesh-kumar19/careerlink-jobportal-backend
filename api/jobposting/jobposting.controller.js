9655ce1864873d670ff1a7f9049a666b52d546ac
dinesh-kumar19 <dineshkumarsdk03@gmail.com>
2025-04-10 19:17:25 +0530

    updated to online connection

--- api/jobposting/jobposting.controller.js
+++ api/jobposting/jobposting.controller.js
@@ -557,8 +557,7 @@ module.exports={
             });  
             // console.log('the token is ' + token);
             const cookieOptions = {
-                expires:new Date(Date.now() + 
-                parseInt(process.env.JWT_COOKIE_EXPIRES) * 24 * 60 * 60 * 1000
+                expires:new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000
                 ),
                 httpOnly: true,
                 secure: process.env.NODE_ENV === 'production',
