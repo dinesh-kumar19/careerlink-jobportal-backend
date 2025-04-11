990d4694e2a6bbe6eb6b64d09ffd83c42295e98b
dinesh-kumar19 <dineshkumarsdk03@gmail.com>
2025-04-11 09:56:14 +0530

    updating live bugs

--- api/jobposting/jobposting.controller.js
+++ api/jobposting/jobposting.controller.js
@@ -559,9 +559,10 @@ module.exports={
             const cookieOptions = {
                 expires:new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000
                 ),
+                secure: true,
                 httpOnly: true,
                 secure: process.env.NODE_ENV === 'production',
-                sameSite: 'strict'
+                sameSite: 'none'
             }
             res.cookie('auth_token', token, cookieOptions);
             return res.status(200).json({
