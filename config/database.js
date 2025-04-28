2ee1f0dabd445af8a4ca399bb94b8f41576134c1
dinesh-kumar19 <dineshkumarsdk03@gmail.com>
2025-04-28 15:11:25 +0530

    backend sleep problem is identified

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
-const { createPool }= require("mysql2");
+// const { createPool }= require("mysql2");
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
@@ -7,6 +7,20 @@ const pool = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
-    connectionLimit:10
+    connectionLimit:10,
+    waitForConnections: true,
+    queueLimit: 0
+});
+pool.on('connection', function (connection) {
+    console.log('MySQL Pool connected: threadId ' + connection.threadId);
+});
+
+pool.on('error', function (err) {
+    console.error('MySQL Pool Error:', err);
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+        console.log('Database connection lost. Reconnecting...');
+    } else {
+        throw err;
+    }
 });
 module.exports = pool;
